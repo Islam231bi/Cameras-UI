a88f1dbb3dd39daab5ed45d68975bd84916488c4
islam231bi <islambassuni@gmail.com>
2022-03-31 14:33:31 +0200

    added killing the process and working

--- Used_Cameras_UI/backend.py
+++ Used_Cameras_UI/backend.py
@@ -1,4 +1,8 @@
+import os
+import signal
 import subprocess
+
+import cv2
 from PySide6.QtWidgets import *
 import multiprocessing as mp
 
@@ -7,6 +11,7 @@ class backend:
     def __init__(self, ui):
         self.ui = ui
         self.isOpen = [0] * 8
+        self.pid = [0] * 8
         # self.url_list = ['rtsp://192.168.1.10:8554/test1', 'rtsp://192.168.1.10:8554/test2',
         #                  'rtsp://192.168.1.10:8554/test3', 'rtsp://192.168.1.10:8554/test4',
         #                  'rtsp://192.168.1.10:8554/test5', 'rtsp://192.168.1.10:8554/test6',
@@ -37,9 +42,11 @@ class backend:
     def updateCameras(self):
         for i in range(8):
             if self.camerasCheckBox[i].isChecked() and self.isOpen[i] == 0:
-                subprocess.Popen(["python3", "cam.py", str(self.url_list[i])])
+                p = subprocess.Popen(["python3", "cam.py", str(self.url_list[i])])
+                self.pid[i] = p.pid
                 self.isOpen[i] = 1
-            if not self.camerasCheckBox[i].isChecked():
+            if not self.camerasCheckBox[i].isChecked() and self.isOpen[i] == 1:
+                os.kill(self.pid[i], signal.SIGTERM)
                 self.isOpen[i] = 0
 
     def reset(self):
